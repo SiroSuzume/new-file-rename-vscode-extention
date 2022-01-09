823101df3dbedf3f5545a519d164944ab5de6fff
sirosuzume <nicorette.kinen@gmail.com>
2022-01-09 10:24:36 +0900

    :recycle: 関数の分割

--- src/extension.ts
+++ src/extension.ts
@@ -1,49 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { renameSync } from 'fs';
-import { commands, ExtensionContext, Uri, window } from 'vscode';
-import { buildNewTestFileName } from './buildNewTestFileName';
-import { findTestingFunctionName } from './findTestingFunctionName';
-import { pickExtensionOfFile as pickExtensionFromFileName } from './pickExtensionOfFile';
-import { pickFileDirectoryFromDocumentFileName } from './pickFileDirectoryFromDocumentFileName';
-import { pickFileNameFromDocumentFileName } from './pickFileNameFromDocumentFileName';
-
-export async function generateTask() {
-  const { activeTextEditor } = window;
-  if (!activeTextEditor) {
-    return;
-  }
-
-  const document = activeTextEditor.document;
-  const fileName = pickFileNameFromDocumentFileName(document.fileName);
-  const extension = pickExtensionFromFileName(fileName);
-  const pathName = pickFileDirectoryFromDocumentFileName(document.fileName);
-
-  if (!/^newFile\.tsx?$/.test(fileName)) {
-    window.showInformationMessage(`'${fileName}' is not unnamed file.`);
-    return;
-  }
-
-  const text = document.getText();
-  const testingFunctionName = findTestingFunctionName(text);
-
-  if (!testingFunctionName) {
-    window.showInformationMessage(`cannot parse testing function name`);
-    return;
-  }
-
-  const newTestFileName = buildNewTestFileName(pathName, testingFunctionName, extension);
-  renameSync(document.fileName, newTestFileName);
-
-  await commands.executeCommand('vscode.open', Uri.file(newTestFileName));
-
-  window.showInformationMessage(`'${document.fileName}' renamed to '${newTestFileName}'`);
-}
+import { commands, ExtensionContext } from 'vscode';
+import { renameUnnamedTestingFile } from './renameUnnamedTestingFile';
 
 export function activate(context: ExtensionContext) {
   const disposable = commands.registerCommand(
     'automatic-jest-generator.generate',
-    generateTask,
+    renameUnnamedTestingFile,
   );
   context.subscriptions.push(disposable);
 }
